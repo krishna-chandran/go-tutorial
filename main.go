9e3af9a94f66d7694bb2c72e35433ee085e76906
Krishnakumar Chandran <mail2krishnachandran@gmail.com>
2024-10-15 13:54:08 -0700

    Basic construct, main package is added with main function, which act as program start

---
+++ main.go
@@ -0,0 +1,18 @@
+/**
+ * @file main.go
+ * @brief Entry point for the Go application.
+ *
+ * The `package main` is required in Go to define an executable program.
+ * The `main` package is a special package in Go that serves as the entry point
+ * for the application. When you run a Go program, the `main` function within
+ * the `main` package is executed.
+ *
+ * Naming the package `main` is necessary for creating an executable file.
+ * If you name the package anything other than `main`, the Go compiler will
+ * generate a package archive (a `.a` file) instead of an executable.
+ */
+package main //
+
+func main() {
+
+}
